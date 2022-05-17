ecd57e4838a3bac9c3c18da64d0902cd92479cd7
David <davidcastellobatet@gmail.com>
2022-05-17 21:52:22 +0200

    commit inicial

---
+++ 01-updatingVariables/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Actualitzant Variables</title>
+</head>
+<body>
+  <h1>La Botiga de Caramels</h1>
+  <h2>Benvingut *nom*</h2>
+  <p>El preu dels caramels es *preu* per paquet</p>
+</body>
+</html>
